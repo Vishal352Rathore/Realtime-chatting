86fdc1c4b93c96693300ecc453177e738f8a09b8
Vishal352Rathore <vishal352rathore@gmail.com>
2024-02-05 16:35:26 +0530

    first commit

---
+++ nodeServer/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const {Server} = require("socket.io");
+
+app.use(cors());
+
+const  server = http.createServer(app);
+
+const io = new Server(server ,{
+    cors : {
+        origin :'http://localhost:3000',
+        methods : ["GET" ,'POST'],
+    }
+})
+
+const users = {};
+
+io.on('connection',(socket) => {
+    socket.on('new-user-joined' , (name) => {
+        console.log("user connected " + name);
+          users[socket.id] = name ;
+          socket.broadcast.emit('user-joined' , name);
+    });
+
+    socket.on('send', (message) =>{
+        socket.broadcast.emit('receive' ,{message:message ,name :users[socket.id]});
+    })
+
+    socket.on('disconnect', () => {
+        console.log("user disconnected");
+    })
+})
+
+server.listen(3001, ()=>{
+    console.log("hello");
+})
