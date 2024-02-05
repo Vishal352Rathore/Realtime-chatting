18938ad26703b9ce28bd655577f31545b9f40f3a
Vishal352Rathore <vishal352rathore@gmail.com>
2024-02-05 18:04:10 +0530

    minor

---
+++ client/src/App.js
@@ -0,0 +1,88 @@
+import './App.css';
+import io from 'socket.io-client';
+import { useState } from 'react';
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+
+  const [userName, setUserName] = useState('');
+  const [userRegister, setUserRegister] = useState(true);
+
+  const [message, setMessage] = useState('');
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    if (userName.trim() === '') {
+      alert('Please fill out the name.');
+      return;
+    }else{
+      socket.emit('new-user-joined', userName);
+      setUserRegister(false);
+      console.log("userName "+ userName);
+    }
+  }
+
+  window.onload = function () {
+
+  const form = document.getElementById('send-container');
+  const messageInput = document.getElementById('messageInp');
+  const messageContainer = document.querySelector('.container');
+
+    
+  const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerHTML = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+
+  form.addEventListener('submit' ,function(e) {
+    console.log("on mess send")
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`you: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+  });
+
+
+socket.on('user-joined', (name) => {
+  append(`${name} joined the chat`, 'center');
+  console.log("name from apend " + name);
+})
+
+socket.on('receive', (data) => {
+  append(`${data.name}: ${data.message}`, 'left')
+})
+};
+
+  return (
+    <div className="App">
+    
+          <div className=''>
+            <h3>Live Chat</h3>
+
+            <p style={{fontWeight : 'bold'}}>Please enter the name to join the chat</p>
+            <form >
+              <input  id='nameInp' type="text" value={userName} onChange={(e) => setUserName(e.target.value)}></input>
+              <button className='btn' type='submit' onClick={(e) => handleClick(e)}>Enter</button>
+            </form>
+          </div>
+               <div className='container'>
+                
+               </div>
+          <div className="send">
+              <form id="send-container" >
+                  <input disabled = {userRegister} type="text" message="messageInp" id='messageInp'  value={message} onChange={(e) => setMessage(e.target.value)}></input>
+                  <button disabled = { message === '' ?true :false} className='btn' type='submit'>Send</button>
+              </form>
+              </div>
+    </div>
+  );
+}
+
+export default App;
