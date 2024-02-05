325843aa2292b71a9e6b1bad903c72f9e9d71ebd
Vishal352Rathore <vishal352rathore@gmail.com>
2024-02-05 21:52:01 +0530

    again url

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import io from 'socket.io-client';
 import { useState } from 'react';
 
-const socket = io.connect("realtime-chatting-server.vercel.app");
+const socket = io.connect("http://realtime-chatting-server.vercel.app");
 
 function App() {
 
