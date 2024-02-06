e41509ec8361f0745915b1741f299faf9a4dbe78
Vishal352Rathore <vishal352rathore@gmail.com>
2024-02-06 14:18:31 +0530

    min

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -25,12 +25,12 @@ io.on('connection',(socket) => {
     socket.on('new-user-joined' , (name) => {
         console.log("user connected " + name);
           users[socket.id] = name ;
-          socket.emit('user-joined' , name);
+          socket.broadcast.emit('user-joined' , name);
     });
 
     socket.on('send', (message) =>{
         console.log("user connected send");
-        socket.emit('receive' ,{message:message ,name :users[socket.id]});
+        socket.broadcast.emit('receive' ,{message:message ,name :users[socket.id]});
    
     })
 
