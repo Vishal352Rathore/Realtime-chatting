2bb642b276e115661395d5323cf0b821ff32814d
Vishal352Rathore <vishal352rathore@gmail.com>
2024-02-11 16:31:07 +0530

    trialsssss

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -11,7 +11,7 @@ const  server = http.createServer(app);
 const io = new Server(server ,{
     cors : {
         // origin :'https://realtime-chatting-server.vercel.app',
-        methods : ["GET" ,'POST'],
+        // methods : ["GET" ,'POST'],
     }
 })
 
